4e88aa109ca6fcd0c2248fd18cee8a2d22be6379
liuyang8091216 <liuyang8091216@126.com>
2016-10-11 11:22:50 +0800

    添加测试程序

---
+++ src/test/java/com/poi/test/EvaluatedRecordActionTest.java
@@ -0,0 +1,29 @@
+package com.poi.test;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.future.domain.AwardRecord;
+
+
+@RunWith(value=SpringJUnit4ClassRunner.class) 
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class EvaluatedRecordActionTest {
+	//private static Logger logger=Logger.getLogger(AwardRecord.class);
+	/*@Resource
+	private EvaluatedRecordService evaluatedRecordService;*/
+	//@Resource
+
+	
+	/*@Test
+	public void TestLikeFindByExpert(){
+		Page_S data=evaluatedRecordService.likeFindByExpertNameOrAllassessment("夏", "30", page);
+		printfPagedata(data);
+	}*/
+	
+	
+
+}
